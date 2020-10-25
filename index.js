2abef195c0c578237bae98210b81526d175ee308
adriantama <tokudatamashiro@gmail.com>
2020-10-25 14:47:20 -0300

    refactorizacion de usuario

--- index.js
+++ index.js
@@ -1,38 +1,12 @@
+const mongoose = require('mongoose')
 const express = require('express')
 const app = express()
 const usuario = require('./routes/usuario')
 app.use(express.json())
-app.use('/api/usuarios', usuario)
+app.use('/api/usuarios/', usuario)
 const port = process.env.PORT || 3007
 app.listen(port, () => console.log('Escuchando puerto: ' +port))
 
-/*
-const date = require('./date');
-const morgan = require('morgan')
-app.use(morgan('tiny'))
-//middleware
-//Te muestra el tiempo en milisegundos
-app.use(date)
-*/
-//middleware
-//Muestra el tipo de petición
-/*app.use(function(req, res, next){
-    console.log('Request Type: ', req.method)
-    next()
-})
-//A este middleware le pasamos el endpoint donde queremos que muestre esta función
-app.use('/api/clientes/list', function(req, res, next){
-    console.log('Request Type: ', req.method)
-    next()
-})
-*/
-/*
-app.use('/api/loggin', function(req, res, next){
-    console.log('loggin')
-    next()
-})
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
-*/
-//app.listen(port, () => console.log('Escuchando puerto: ' +port))
+mongoose.connect('mongodb://localhost/canchitApp', {useNewUrlParser:true, useUnifiedTopology: true, useFindAndModify: false})
+    .then(() => console.log('Conectado a MongoDb'))
+    .catch(erro => console.log('No se ha conectado a MongoDb'))
--- routes/usuario.js
+++ routes/usuario.js
@@ -1,141 +1,81 @@
+const mongoose = require('mongoose')
 const express = require('express')
-//const app = express()
+const Usuario = require('../data/usuario')
 const router = express.Router()
 const { body, validationResult } = require('express-validator');
 
-var usuarios = [
-    {dni: 93688330, nombre: 'Adrian', apellido: 'Tamashiro', email: 'adriantama@email.com', fecha_nacimiento: '12-03-1991'},
-    {dni: 12345678, nombre: 'Cristian', apellido: 'Ciz', email: 'crisciz@email.com', fecha_nacimiento: '16-10-1992'},
-    {dni: 12121212, nombre: 'Mariam', apellido: 'Sleiman', email: 'marsleiman@email.com', fecha_nacimiento: '04-10-1995'},
-    {dni: 99893289, nombre: 'Alejandro', apellido: 'Facchinelli', email: 'alefacchinelli@email.com', fecha_nacimiento: '21-08-1992'},
-    {dni: 43289128, nombre: 'Rocio', apellido: 'Vargas', email: 'rociovargas@email.com', fecha_nacimiento: '21-05-1992'}
-];
-
-router.get('/list', (req, res) => {
-    res.send(['Adrian', 'Cris', 'Mar', 'Rocio', 'Ale'])
-})
- 
-router.get('/id/:id', (req, res) => {
-    res.send(req.params.id)
-})
-
-router.get('/:nombre/:apellido', (req, res) => {
-    //res.send(req.params.nombre)
-    //res.send(req.params.apellido)
-    //Lo envía en formato objeto en el de req.params
-    res.send(req.params)
-})
-
-router.get('/', (req, res) => {
+router.get('/', async(req, res) => {
+    const usuarios = await Usuario.find()
     res.send(usuarios)
 })
 
-router.get('/:email', (req, res) => {
-    const user = usuarios.find(user => user.email === req.params.email)
-
-    if(!user){
-        res.status(404).send('No tenemos ningún usuario con ese email')
-    } else{
-        res.send(user)
-    }
-})
-
-
-router.post('/', (req, res) =>{
-    var user = {
-        dni: req.body.dni,
-        nombre: req.body.nombre,
-        apellido: req.body.apellido,
-        email: req.body.email,
-        fecha_nacimiento: req.body.fecha_nacimiento
-    }
-    usuarios.push(user)
-    res.status(201).send(user)
-
-})
-
-//Con filtros o restricciones
-router.post('/2/', (req, res) =>{
-    //Significa diferente de vacio o nombre menor a 11 caracteres
-    if(!req.body.nombre || req.body.nombre.length > 10){
-        res.status(400).send('Introduce el nombre correcto')
-        return
-    }
-    if(!req.body.apellido || req.body.apellido.length > 10){
-        res.status(400).send('Introduce el apellido correcto')
-        return
-    }
-    var user = {
-        dni: req.body.dni,
-        nombre: req.body.nombre,
-        apellido: req.body.apellido,
-        email: req.body.email,
-        fecha_nacimiento: req.body.fecha_nacimiento
-    }
-    usuarios.push(user)
-    res.status(201).send(user)
-
+router.get('/:id', async(req, res) =>{
+    const usuario = await Usuario.findById(req.params.id)
+    if(!usuario) return res.status(404).send('No se ha encontrado al usuario con ese ID')
+    res.send(usuario)
 })
 
 
 //Usando express-validator
 //En este caso valido que el email sea un email con el @ y que el nombre sea mínimo de 3 caracteres
-router.post('/3/', [
+router.post('/', [
     body('email').isEmail(),
     body('nombre').isLength({min: 3})
-], (req, res) =>{
+], async(req, res) =>{
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
     
-    var user = {
+    const usuario = new Usuario({
         dni: req.body.dni,
         nombre: req.body.nombre,
         apellido: req.body.apellido,
         email: req.body.email,
         fecha_nacimiento: req.body.fecha_nacimiento
-    }
-    usuarios.push(user)
-    res.status(201).send(user)
+    })
 
+    const result = await usuario.save()
+    res.status(201).send(result)
 })
 
 
-router.put('/:dni', [
+router.put('/:id', [
     body('email').isEmail(),
     body('nombre').isLength({min: 3})
-], (req, res) =>{
+], async(req, res) =>{
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
 
-    const user = usuarios.find(user => user.dni === parseInt(req.params.dni))
+    const usuario = await Usuario.findByIdAndUpdate(req.params.id, {
+        dni: req.body.dni,
+        nombre: req.body.nombre,
+        apellido: req.body.apellido,
+        email: req.body.email,
+        fecha_nacimiento: req.body.fecha_nacimiento
+    },
+    {
+        new: true
+    })
 
-    if(!user){
-        return res.status(404).send('El usuario con ese dni no existe')
-    }
-
-    user.nombre = req.body.nombre
-    user.apellido = req.body.apellido
-    user.email = req.body.email
-    user.fecha_nacimiento = req.body.fecha_nacimiento    
+    if(!usuario){
+        return res.status(404).send('El usuario con ese id no existe')
+    }   
     
     res.status(204).send()
 
 })
 
 
-router.delete('/:dni', (req, res) => {
-    const user = usuarios.find(user => user.dni === parseInt(req.params.dni))
+router.delete('/:id', async(req, res) => {
+    const usuario = await Usuario.findByIdAndDelete(req.params.id)
 
-    if(!user){
-        return res.status(404).send('Ese dni no existe')
+    if(!usuario){
+        return res.status(404).send('Ese id no existe')
     }
 
-    const index = usuarios.indexOf(user)
-    usuarios.splice(index, 1)
     res.status(200).send('Usuario eliminado')
 })
 
