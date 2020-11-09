0aabfcb1130d0520ad2b2a923995a7de4d68e87b
Alejandro Facchinelli <alejandro.facchinelli@gmail.com>
2020-11-09 14:29:51 -0300

    creacion env

--- data/conexionMongo.js
+++ data/conexionMongo.js
@@ -1,9 +1,9 @@
 const mongoClient = require('mongodb').MongoClient;
 const chalk = require('chalk');
-//const dotenv = require('dotenv').config(); 
+const dotenv = require('dotenv').config(); 
 
-const uriMongo = 'mongodb+srv://admin:admin@cluster0.uyw4y.mongodb.net/canchitAppDB?retryWrites=true&w=majority';
-//const uriMongo = process.env.URIMONGO;
+// const uriMongo = 'mongodb+srv://admin:admin@cluster0.uyw4y.mongodb.net/canchitAppDB?retryWrites=true&w=majority';
+const uriMongo = process.env.URIMONGO;
 console.log(uriMongo);
 
 const client = new mongoClient(uriMongo, { useUnifiedTopology: true, useNewUrlParser: true });
